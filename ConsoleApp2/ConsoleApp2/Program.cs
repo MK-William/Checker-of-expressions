7345a2471ca22b2cb31ed0d1470c2f802221ecce
Maikon William <maikonwilliam08@gmail.com>
2023-02-23 08:32:39 -0300

    Checker of expressions

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex029 {
+    class Program {
+        static void Main(string[] args) 
+        {
+            int aspas_abertas = 0, aspas_fechadas = 0;
+
+            List<char> aspas = new List<char>();
+            List<char> aspas2 = new List<char>();
+
+            Console.WriteLine("Input a expression: ");
+            string expression = Console.ReadLine();
+
+            foreach (char letter  in expression)
+            {
+                if (letter == '(')
+                {
+                    aspas.Add('(');  
+                }
+                if (letter == ')')
+                {
+                    aspas2.Add(')');
+                }
+            }
+            foreach (char aspa in aspas2)
+            {
+             if (aspas.Count > 0)
+                {
+                    aspas.RemoveAt(aspas.Count-1);
+                }                               
+            }
+
+            if (aspas.Count > 0  ) {
+                Console.WriteLine("A operação é Invalida");
+            }
+            else if (aspas.Count == 0 ) {
+                Console.WriteLine("A operação é valida");
+            }
+        }
+    }
+}
